17ef1053fea13cc8f6be03e701f2e81ee4c180ce
Navya Mogili <navyamogili413@gmail.com>
2023-04-16 19:39:09 -0600

    ADDing SQL

---
+++ SQL_prep/4_SQL.sql
@@ -0,0 +1,6 @@
+--1. From the following table, write a SQL query to calculate total purchase amount of all orders. Return total purchase amount.   Go to the editor
+--Sample table: orders
+SELECT sum(purch_amt)
+from Orders;
+
+========================================================================================================================
